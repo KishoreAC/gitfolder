d525f0c9a9f31b342ed5b80461906c131a832488
KishoreAC <kchandradasac@gmail.com>
2021-05-17 16:40:30 +0530

    kept one liner code; 17-May-2021 16:40; Kishore

--- kishhello.c
+++ kishhello.c
@@ -2,17 +2,5 @@
 
 int main () {
 	printf("Hello World \n");
-	printf("This is modified code\n");
-	printf("This change is for GitHub\n");
-	printf("Enabled github\n");
-	printf("Re ran the jenkin's image with saved configs\n");
-	printf("One more re ran jenkins\n");
-	printf("New line\n");
-	printf("one more?\n");
-	printf("Test one more time \n");
-	printf("Test e-mail notification");
-	printf("Hi All final test?\n");
-	printf("Hi all emails fix\n");
-	printf("all final with email fix\n");
 	return(0);
 }
